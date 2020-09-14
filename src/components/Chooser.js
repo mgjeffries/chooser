e8a8f2d43af99ce8fae844e6d1b646f6f5d4bf85
mgjeffries <gib.jeffries@gmail.com>
2020-09-14 11:57:42 -0500

    add login

--- src/components/Chooser.js
+++ src/components/Chooser.js
@@ -1,9 +1,12 @@
 import React from "react"
 import { Route, Redirect } from "react-router-dom"
 import "./Chooser.css"
+import { ApplicationViews } from "./ApplicationViews"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
 
 export const Chooser = () => {
-  <>
+  return <>
     <Route render={() =>{
       if (localStorage.getItem("chooser_user")) {
         return <Route render={props => <ApplicationViews {...props}/>} />
---
+++ src/components/auth/Logout.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export const Logout = (props) => {
+  localStorage.removeItem("chooser_user")
+  props.history.push("/login")
+
+  return <></>
+}
