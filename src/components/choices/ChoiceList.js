69c2b044d96ce90fee0a11bc43345447a41b82f0
mgjeffries <gib.jeffries@gmail.com>
2020-09-14 14:59:42 -0500

    list choices

--- src/components/choices/ChoiceList.js
+++ src/components/choices/ChoiceList.js
@@ -1,17 +1,27 @@
 import React, { useContext, useEffect } from "react"
 import { ChoiceContext } from "./ChoiceProvider"
+import { Link } from "react-router-dom"
 
 const defaultWeightId = 1
 
 export const ChoiceList = () => {
-  const { choice, getChoices, addChoice } = useContext(ChoiceContext)
+  const { choices, getChoices, addChoice } = useContext(ChoiceContext)
 
   useEffect( () => {
     getChoices()
   }, [])
 
   return <> 
-  <section className="choice">
+  <section className="choices">
+    <div className="choice__list">
+    {
+        choices.map(choice => {
+            return <Link key={choice.id} to={`/choices/${choice.id}`}>
+                <h3>{choice.name}</h3>
+            </Link>
+        })
+    }
+    </div>
     <div className="choice__add">
       <button 
           onClick={evt => {
