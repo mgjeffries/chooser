8da6cf27c68d6d5e1457b44321debfd22f7fbd36
mgjeffries <gib.jeffries@gmail.com>
2020-09-14 15:13:58 -0500

    add choice detail

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -4,15 +4,19 @@ import { WeightList } from "./weights/WeightList"
 import { WeightProvider } from "./weights/WeightProvider"
 import { ChoiceProvider } from "./choices/ChoiceProvider"
 import { ChoiceList } from "./choices/ChoiceList"
+import { ChoiceDetail } from "./choices/ChoiceDetail"
 
 export const ApplicationViews = (props) => {
   return (
     <>
-      <Route exact path="/">
-        <ChoiceProvider>
-          <ChoiceList />
-        </ChoiceProvider>
-      </Route>
+      <ChoiceProvider>
+        <Route exact path="/">
+            <ChoiceList />
+        </Route>
+        <Route path="/choices/:choiceId(\d+)" render={
+            props => <ChoiceDetail {...props} />
+        } />
+      </ChoiceProvider>
 
       <Route exact path="/weight">
         <WeightProvider>
