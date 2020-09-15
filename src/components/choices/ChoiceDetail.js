3838d2b712e2b09addc4c62a9f45c5ba7d8f466b
mgjeffries <gib.jeffries@gmail.com>
2020-09-15 10:36:37 -0500

    allow user to edit choice name

--- src/components/choices/ChoiceDetail.js
+++ src/components/choices/ChoiceDetail.js
@@ -1,11 +1,14 @@
-import React, { useContext, useEffect, useState } from "react"
+import React, { useContext, useEffect, useState, useRef } from "react"
 import { ChoiceContext } from "./ChoiceProvider"
 import { WeightList } from "../weights/WeightSelect"
 
 
 export const ChoiceDetail = (props) => {
-  const { choices, getChoices } = useContext(ChoiceContext)
+  const { choices, getChoices, editChoice } = useContext(ChoiceContext)
   const [ choice, setChoice ] = useState({})
+  const [ isChoiceNameChanging, setIsChoiceNameChanging ] = useState(false)
+
+  const choiceRef = useRef(choice)
 
   useEffect( () => {
     getChoices()
@@ -16,10 +19,38 @@ export const ChoiceDetail = (props) => {
       setChoice(choice)
   }, [choices])
 
+  const changeName = () => {
+    const newChoice = choice
+    newChoice.name = choiceRef.current.value 
+    editChoice(newChoice)
+    setIsChoiceNameChanging(false)
+  }
+
   return <> 
   <section className="choice">
     <div>
-      {choice.name}
+      { 
+      (isChoiceNameChanging)
+      ? <>
+      <input type="text" defaultValue={choice.name} ref={choiceRef}></input>
+      <button type="submit"
+            onClick={evt => {
+                evt.preventDefault() // Prevent browser from submitting the form
+                changeName()
+            }}
+            className="btn btn-primary">
+            Save
+        </button>
+      </>
+      : <>
+      <div onClick={evt => {
+          setIsChoiceNameChanging(true)
+        }}
+        className="btn">
+          {choice.name}
+      </div>
+      </>
+      }
     </div>
     <WeightList {...props} />
 
