cb94962c11e69bd28dbcbc696801c14037036bde
mgjeffries <gib.jeffries@gmail.com>
2020-09-16 14:14:32 -0500

    show options in table

--- src/components/choices/ChoiceDetail.js
+++ src/components/choices/ChoiceDetail.js
@@ -71,11 +71,7 @@ export const ChoiceDetail = (props) => {
     
     <WeightList {...props} />
     
-    {
-      choiceOptions.map(cO => {
-        return <Option option={cO} key={cO.id}/>
-      })
-    }
+    
     
     <Table striped bordered hover>
       <thead>
@@ -93,23 +89,20 @@ export const ChoiceDetail = (props) => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
+        {
+          choiceOptions.map(cO => {
+            return (
+              <tr key={cO.id}>
+                <td>
+                  <Option option={cO} />
+                </td>
+                <td>sample rating 1</td>
+                <td>sample rating 2</td>
+                <td>sample rating 3</td>
+              </tr>
+            )
+          })
+        }
       </tbody>
     </Table>
     <AddOption {...props} />
