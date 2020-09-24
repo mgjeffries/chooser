9e133bc6ac97d12950e0dadd505cce1f8f30cc31
mgjeffries <gib.jeffries@gmail.com>
2020-09-24 11:20:16 -0500

    add bootswatch sandstone theme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-import { Chooser } from "./components/Chooser"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Chooser } from "./components/Chooser";
+import "./bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +12,7 @@ ReactDOM.render(
       <Chooser />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
