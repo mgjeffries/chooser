d6105e4476e9e0b84b03a06d7cc4479baa04afd4
mgjeffries <gib.jeffries@gmail.com>
2020-09-25 16:17:05 -0500

    add container and step indicator text to flows

--- src/components/flow/FlowName.js
+++ src/components/flow/FlowName.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { ChoiceContext } from "../choices/ChoiceProvider";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 export const FlowName = (props) => {
   const { choices, getChoices, editChoice } = useContext(ChoiceContext);
@@ -25,31 +26,33 @@ export const FlowName = (props) => {
 
   return (
     <>
-      <h1 className="choice__name">What is on your mind?</h1>
-      <div>Examples:</div>
-      <div>•New Car?</div>
-      <div>•Where to work?</div>
-      <div>•Which Cell Carrier</div>
-      <div>•Where to go for Vacation?</div>
-      <Form>
-        <Form.Label>Name of Choice</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Who, What, When, Where?"
-          name="name"
-          onChange={handleControlledInputChange}
-        />
-      </Form>
-      <Button
-        variant="primary"
-        onClick={(clickEvent) => {
-          editChoice(choice).then((responseChoice) =>
-            props.history.push(`/choices/${choice.id}/options`)
-          );
-        }}
-      >
-        next
-      </Button>
+      <Container>
+        <h2 className="choice__name">What's on your mind? (Step 1/3)</h2>
+        <div>Examples:</div>
+        <div>•New Car?</div>
+        <div>•Where to work?</div>
+        <div>•Which Cell Carrier to use?</div>
+        <div>•Where to go for Vacation?</div>
+        <Form>
+          <Form.Label>Name of Choice</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Who, What, When, Where?"
+            name="name"
+            onChange={handleControlledInputChange}
+          />
+        </Form>
+        <Button
+          variant="primary"
+          onClick={(clickEvent) => {
+            editChoice(choice).then((responseChoice) =>
+              props.history.push(`/choices/${choice.id}/options`)
+            );
+          }}
+        >
+          next
+        </Button>
+      </Container>
     </>
   );
 };
